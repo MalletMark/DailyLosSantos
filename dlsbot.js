2e02b882d6f3f0828d9ec6fa1513a12c01ba482a
MalletMark <Mark Real>
2019-05-11 23:38:43 -0700

    pix troll

--- dlsbot.js
+++ dlsbot.js
@@ -3,9 +3,12 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
+var weedDate = Date.now();
+const weedWords = ['weed', 'smoke', 'bowl', 'kush', 'bong', 'drug', 'pot', 'dank', 'thc', 'blunt', 'joint', '420'];
 
 client.once('ready', () => {
-	console.log('Ready!');
+    console.log('Ready!');
+    weedDate = Date.now();
 });
 
 client.login(process.env.API_CLIENT_TOKEN);
@@ -13,6 +16,15 @@ client.login(process.env.API_CLIENT_TOKEN);
 client.on('message', message => {
     if (message.content === '!testbot'){
         message.channel.send('works')
+    }
+
+    if (message.author.username == "Pixstrad" && weedWords.some(word => message.content.includes(word)))
+    {
+        const qChannel = message.guild.channels.find(ch => ch.name === "test");
+        var weedSince = Math.round((Date.now() - weedDate) / 1000);
+        console.log(weedSince);
+        message.channel.send("It's been " + weedSince.toString() + " seconds since Pixtrad has mentioned weed.");
+        weedDate = Date.now();
     }
 
     if (message.content.substring(0, 6) === '!quote' &&

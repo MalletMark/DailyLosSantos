e3d28ab880ccf7b794c2c71ce0970c214ec7190b
MalletMark <Mark Real>
2019-05-12 18:58:57 -0700

    recap bot

--- dlsbot.js
+++ dlsbot.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const { Client, RichEmbed } = require('discord.js');
+
 var weedDate = Date.now();
 const weedWords = ['weed', 'smoke', 'bowl', 'kush', 'bong', 'drug', 'pot', 'dank', 'thc', 'blunt', 'joint', '420'];
 
@@ -13,9 +14,52 @@ client.once('ready', () => {
 
 client.login(process.env.API_CLIENT_TOKEN);
 
+function hasReaction(reactions, emojiName)
+{
+    if (reactions.length > 0) {
+        return reactions.some(function(r) {
+            return r._emoji.name == emojiName;
+        });
+    }
+    return false;
+}
+
 client.on('message', message => {
     if (message.content === '!testbot'){
         message.channel.send('works')
+    }
+
+    if (message.content.substring(0,6) === '!recap')
+    {
+        var recapNum = Number(message.content.substring(7));
+        var rCount = 0;
+        console.log(recapNum);
+        var messagesQueue = [];
+
+        message.channel.fetchMessages({limit: 100})
+        .then(messages => {
+            messages.array().every(function(m)
+            {
+                if (hasReaction(m.reactions.array(), 'recap'))
+                {
+                    if (++rCount > recapNum) return false;
+                    messagesQueue.push(m);
+                    return true;
+                }
+                return true;
+            })
+
+            if (messagesQueue.length > 0)
+            {
+                recapNum = (recapNum > messagesQueue.length) ? messagesQueue.length : recapNum;
+                message.channel.send('Here are the last ' + recapNum.toString() + ' recaps I could find!')
+                messagesQueue.reverse();
+                messagesQueue.forEach(function(m)
+                {
+                    m.channel.send(m.url);
+                })
+            }
+        });
     }
 
     if (message.author.username == "Pixstrad" && weedWords.some(word => message.content.includes(word)))
@@ -40,7 +84,7 @@ client.on('message', message => {
         const embed = new RichEmbed()
         .setFooter("- " + person)
         .setColor(0xFF0000)
-        .setDescription(body)
+        .setDescription(body);
 
         qChannel.send(embed);
     }

5a347b4452eab418f33b7196be7bdd178f4c4a5f
MalletMark <Mark Real>
2019-05-25 15:15:22 -0700

    staffbot fix 3

--- dlsbot.js
+++ dlsbot.js
@@ -621,7 +621,7 @@ function staffBot(message) {
         message.channel.send(`Please visit ${message.guild.channels.get('581136851654541331').toString()}`);
         return;
     }
-    const cName = message.content.substring(11);
+    const cName = message.content.substring(6);
 
     MongoClient.connect(mongoUrl, function(err, client) {
         const col = client.db(mongoDbName).collection('dls_staff');
